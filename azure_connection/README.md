4b30ce1074f6ebe0f215004a5e7a465d3c0b620c
Carlos José <129355214+carljos97@users.noreply.github.com>
2023-03-30 01:44:49 -0300

    Add files via upload

---
+++ azure_connection/README.md
@@ -0,0 +1,30 @@
+# Commands for implementing local and cloud functions
+
+To be able to implement the functions we need, first of all, to have access to the database that is in the Azure. The code deployed checks where it is running: locally or in the cloud. So to make it possible to run the code in the cloud, it is necessary to follow the steps below:
+
+[Download the following: Download Azure CLI](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-cli)
+
+In Powershell, run the following commands:
+
+Azure CLI login: 
+`az login`
+
+Generate access token (1 hour):
+```
+az account get-access-token --
+resource=https://database.windows.net/ --query accessToken
+```
+The access code must be added as the db_token variable in the code, allowing access for 1 hour.
+
+The implemented code is already functional for running in the cloud, requiring no adjustments.
+
+
+**Installation support documentation**
+
+[Structuring functions from scratch](https://www.youtube.com/watch?v=_Xr_SxDeub4)
+
+[Install pyodbc](https://learn.microsoft.com/pt-br/sql/connect/python/pyodbc/python-sql-driver-pyodbc?view=sql-server-ver16)
+
+[Install pyodbc drive](https://learn.microsoft.com/pt-br/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16)
+
+[How to connect to Azure SQL?](https://techcommunity.microsoft.com/t5/apps-on-azure-blog/how-to-connect-azure-sql-database-from-python-function-app-using/ba-p/3035595)
---
+++ azure_connection/connections.py
@@ -0,0 +1,92 @@
+'''
+Connecting to database using username and password
+'''
+
+import pyodbc 
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+server = 'put_your_server_name_here' 
+database = 'put_your_database_name_here' 
+username = 'put_your_username_here' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';Authentication=ActiveDirectoryInteractive;ENCRYPT=yes')
+cursor = cnxn.cursor()
+
+
+
+'''
+Connecting to database using username and password, but using a different aproach to define the connection string
+'''
+
+import textwrap
+import pyodbc
+
+# Specify the driver
+driver = '{ODBC Driver 18 for SQL Server}'
+
+# Specify the Server Name and Database Name
+server_name = 'put_your_server_name_here'
+database_name = 'put_your_database_name_here'
+
+# Create our server URL
+server = '{server_name}.database.windows.net,1433'.format(server_name=server_name)
+
+# Define username & password
+username = "put_your_username_here"
+password = "put_your_password_here"
+
+# Create the full connection string
+connection_string = textwrap.dedent('''
+    Driver={driver};
+    Server={server};
+    Database={database};
+    Uid={username};
+    Pwd={password};
+    Encrypt=yes;
+    TrustServerCertificate=no;
+    Connection Timeout=30;
+'''.format(
+    driver=driver,
+    server=server,
+    database=database_name,
+    username=username,
+    password=password
+))
+
+# Create a new PYODBC Connection Object.
+cnxn: pyodbc.Connection = pyodbc.connect(connection_string)
+
+# Create a new Cursor object from the connection
+crsr: pyodbc.Cursor = cnxn.cursor()
+
+#Close the connection once we are done
+
+'''
+Connecting to database using a token
+'''
+
+import logging
+import azure.functions as func
+import os
+import pyodbc
+import struct
+
+def db_connect():
+    server = 'put_your_server_name_here' 
+    database = 'put_your_database_name_here' 
+    db_token = 'put_the_generated_token_here'
+    #condition to check if the code is running locally or in the cloud
+    if os.getenv("MSI_SECRET"):
+        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';Authentication=ActiveDirectoryMsi')
+    else:
+        SQL_COPT_SS_ACCESS_TOKEN = 1256
+        
+        exptoken = b''
+        for i in bytes(db_token, "UTF-8"):
+            exptoken += bytes({i})
+            exptoken += bytes(1)
+
+        tokenstruct = struct.pack("=i", len(exptoken)) + exptoken
+        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database, attrs_before = { SQL_COPT_SS_ACCESS_TOKEN:tokenstruct })
+
+    return cnxn.cursor()
